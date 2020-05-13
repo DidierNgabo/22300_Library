269ce9ad56c9a289f3e46e686076ce96ec4c3dda
Didier Ngabo Uwayo <diddynu2000@gmail.com>
2020-05-13 12:23:17 +0200

    first commit

--- src/views/Checkinout.java
+++ src/views/Checkinout.java
@@ -5,6 +5,10 @@
  */
 package views;
 
+import java.util.Date;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -19,6 +23,9 @@ public class Checkinout extends javax.swing.JInternalFrame {
      */
     public Checkinout() {
         initComponents();
+        bookCombo();
+        clientCombo();
+        table();
     }
 
     /**
@@ -38,14 +45,12 @@ public class Checkinout extends javax.swing.JInternalFrame {
         date = new com.toedter.calendar.JDateChooser();
         checkinbtn = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        table = new javax.swing.JTable();
 
         jLabel1.setText("Client name");
 
-        clientcombo.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-
         jLabel2.setText("Book title");
-
-        bookcombo.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
         jLabel3.setText("Date");
 
@@ -57,67 +62,179 @@ public class Checkinout extends javax.swing.JInternalFrame {
         });
 
         jButton2.setText("Check out");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        table.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Id", "Client Name", "Book Title", "status", "date"
+            }
+        ));
+        table.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tableMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(table);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addGap(65, 65, 65)
+                .addComponent(checkinbtn)
+                .addGap(51, 51, 51)
+                .addComponent(jButton2)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(34, 34, 34)
+                        .addGap(8, 8, 8)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel3))
-                        .addGap(31, 31, 31)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(clientcombo, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(bookcombo, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(date, javax.swing.GroupLayout.DEFAULT_SIZE, 106, Short.MAX_VALUE)))
+                            .addComponent(bookcombo, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(date, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(65, 65, 65)
-                        .addComponent(checkinbtn)
-                        .addGap(51, 51, 51)
-                        .addComponent(jButton2)))
-                .addContainerGap(124, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(clientcombo, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 408, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(25, 25, 25))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(63, 63, 63)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(clientcombo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(bookcombo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel3)
-                    .addComponent(date, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(39, 39, 39)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(63, 63, 63)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1)
+                            .addComponent(clientcombo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(bookcombo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel3)
+                            .addComponent(date, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(39, 39, 39))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(55, 55, 55)))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(checkinbtn)
                     .addComponent(jButton2))
-                .addContainerGap(57, Short.MAX_VALUE))
+                .addContainerGap(161, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+    public void bookCombo() {
+        Session s = util.Connection.getSessionFactory().openSession();
+        Transaction tr = s.beginTransaction();
+        List<models.Books> bc = s.createQuery(" from Books ").list();
 
+        for (models.Books ls : bc) {
+            System.out.println(ls.getTitle());
+            String name = ls.getTitle();
+            bookcombo.addItem(name);
+        }
+
+    }
+     public void clientCombo() {
+        Session s = util.Connection.getSessionFactory().openSession();
+        Transaction tr = s.beginTransaction();
+        List<models.Clients> bc = s.createQuery(" from Clients ").list();
+
+        for (models.Clients ls : bc) {
+            System.out.println(ls.getFirstname() + " "+ ls.getLastname());
+            String name = ls.getFirstname() + " " + ls.getLastname();
+            clientcombo.addItem(name);
+        }
+
+    }
+    public void table() {
+        Session s = util.Connection.getSessionFactory().openSession();
+        Transaction tr = s.beginTransaction();
+        List<models.Checkinout> list = s.createQuery("from Checkinout").list();
+        tr.commit();
+        DefaultTableModel model = new DefaultTableModel();
+        model = (DefaultTableModel) table.getModel();
+        model.getDataVector().removeAllElements();
+
+        Object[] obj = new Object[5];
+
+        for (models.Checkinout op : list) {
+            obj[0] = op.getId();
+            obj[1] = op.getBookTitle();
+            obj[2] = op.getClientName();
+            obj[3] = op.getStatus();
+            obj[4] = op.getCheckinDate();
+
+            model.addRow(obj);
+            System.out.println("row is displayed");
+
+        }
+        s.close();
+
+    }
     private void checkinbtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_checkinbtnActionPerformed
         // TODO add your handling code here:
         Session s= util.Connection.getSessionFactory().openSession();
         Transaction tr=s.beginTransaction();
         models.Checkinout operation=new models.Checkinout();
-        operation.setBookTitle(title);
-        operation.setClientName(title);
-        operation.setStatus(title);
-        operation.setCheckinDate(null);
-        operation.setCheckoutDate(null);
+        operation.setBookTitle(bookcombo.getSelectedItem().toString());
+        operation.setClientName(clientcombo.getSelectedItem().toString());
+        operation.setStatus("Check in");
+        operation.setCheckinDate(date.getDate());
+        operation.setCheckoutDate(date.getDate());
+        
+        s.save(operation);
+        tr.commit();
+        table();
+        JOptionPane.showMessageDialog(null, "operation saved");
+        s.close();
     }//GEN-LAST:event_checkinbtnActionPerformed
+    int  opId;
+    private void tableMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tableMouseClicked
+        // TODO add your handling code here:
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        int selectedRow = table.getSelectedRow();
+      
+        opId= (int) model.getValueAt(selectedRow, 0);
+        clientcombo.setSelectedItem(model.getValueAt(selectedRow, 2));
+        bookcombo.setSelectedItem(model.getValueAt(selectedRow, 3));
+        date.setDate((Date) model.getValueAt(selectedRow, 4));
+    }//GEN-LAST:event_tableMouseClicked
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        Session s = util.Connection.getSessionFactory().openSession();
+        Transaction tr = s.beginTransaction();
+        models.Checkinout operation = (models.Checkinout) s.get(models.Checkinout.class, opId);
+
+        operation.setCheckinDate(date.getDate());
+        operation.setCheckoutDate(date.getDate());
+        operation.setStatus("check out");
+        s.update(operation);
+        tr.commit();
+        JOptionPane.showMessageDialog(null, "book returned successfully");
+        table();
+        s.close();
+    }//GEN-LAST:event_jButton2ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -129,5 +246,7 @@ public class Checkinout extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable table;
     // End of variables declaration//GEN-END:variables
 }
--- src/views/Home.java
+++ src/views/Home.java
@@ -31,6 +31,7 @@ public class Home extends javax.swing.JFrame {
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         auth = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
@@ -57,6 +58,14 @@ public class Home extends javax.swing.JFrame {
             }
         });
         jMenu1.add(auth);
+
+        jMenuItem3.setText("checkinout");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem3);
 
         jMenuBar1.add(jMenu1);
 
@@ -119,6 +128,13 @@ public class Home extends javax.swing.JFrame {
          auth.show();
     }//GEN-LAST:event_authActionPerformed
 
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        // TODO add your handling code here:
+        Checkinout op=new Checkinout();
+        desktop.add(op);
+        op.show();
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -163,5 +179,6 @@ public class Home extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     // End of variables declaration//GEN-END:variables
 }
